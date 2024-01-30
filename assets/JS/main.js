7ff7aaa73241d337e812c5f2e2dd5d3adf64eefc
moRezk12 <mohanedrazk54@gmail.com>
2024-01-31 00:25:55 +0200

    commit

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -71,14 +71,88 @@ window.addEventListener('scroll' , changebg);
 
 // 
 
-// let deposit = document.querySelector('#deposit');
-// let withdraw = document.querySelector('#withdraw');
+let depositbtn = document.querySelector('#deposit-btn');
+let withdrawbtn = document.querySelector('#withdraw-btn');
 
-// deposit.addEventListener("click" , () =>{
-//     deposit.classList.remove('none'); 
-//     withdraw.classList.add('none'); 
-// })
-// withdraw.addEventListener("click" , () =>{
-//     withdraw.classList.remove('none'); 
-//     deposit.classList.add('none'); 
-// })
+let deposit = document.querySelector('#deposit');
+let withdraw = document.querySelector('#withdraw');
+
+depositbtn.addEventListener("click" , () =>{
+    depositbtn.style.background = '#00bfff'; 
+    withdrawbtn.style.background = '#26354d';
+
+    deposit.classList.remove('hide'); 
+    withdraw.classList.add('hide'); 
+});
+
+
+withdrawbtn.addEventListener("click" , () =>{
+    withdrawbtn.style.background = '#00bfff'; 
+    depositbtn.style.background = '#26354d';
+
+    withdraw.classList.remove('hide'); 
+    deposit.classList.add('hide'); 
+});
+
+// In Telephone 
+let depositbtn2 = document.querySelector('#deposit-btn2');
+let withdrawbtn2 = document.querySelector('#withdraw-btn2');
+
+
+let deposit2 = document.querySelector('.table-tablet-deposit');
+let withdraw2 = document.querySelector('.table-tablet-withdraw');
+
+depositbtn2.addEventListener("click" , () =>{
+    depositbtn2.style.background = '#00bfff'; 
+    withdrawbtn2.style.background = '#26354d';
+
+    deposit2.style.display = 'block';
+    withdraw2.style.display = 'none';
+});
+
+
+withdrawbtn2.addEventListener("click" , () =>{
+    withdrawbtn2.style.background = '#00bfff'; 
+    depositbtn2.style.background = '#26354d';
+
+    deposit2.style.display = 'none';
+    withdraw2.style.display = 'block';
+});
+
+
+// Accordion
+
+const accordionContent = document.querySelectorAll('.accordion-content');
+
+accordionContent.forEach((item , index) =>{
+    let header = item.querySelector('.accordion-head');
+
+    header.addEventListener("click" , () =>{
+        item.classList.toggle("open");
+
+        let description = item.querySelector('.description');
+        if(item.classList.contains('open')){
+            description.style.height = `${description.scrollHeight}px`;
+            item.querySelector('i').classList.replace('fa-plus','fa-minus');
+
+
+        }else{
+            description.style.height = '0px';
+            item.querySelector('i').classList.replace('fa-minus','fa-plus');
+        }
+        removeOpen(index);
+        console.log(item);
+    });
+    
+});
+
+function removeOpen (index1){
+    accordionContent.forEach((item2,index2) =>{
+        if(index1 != index2){
+            item2.classList.remove('open');
+            let des = item2.querySelector('.description');
+            des.style.height = '0px';
+            item2.querySelector('i').classList.replace('fa-minus' , 'fa-plus');
+        }
+    });
+};
